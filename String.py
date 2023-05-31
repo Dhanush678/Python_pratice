69c20ce0c66de95aef3d70f5d32b351995b05d09
dhanush678 <dhanugowda200326@gmail.com>
2023-05-31 18:36:01 +0530

    String finished

---
+++ String.py
@@ -0,0 +1,26 @@
+#Learning the python string formatting
+name="Dhanush"
+
+
+print(name.title())
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+
+
+#cancatenation
+middle=" gowda "
+last=" U"
+full_name=name+middle+last
+print(full_name)
+#whitespcae and newline
+print(name+"\n")
+print(middle+"\n")
+print(last+"\n")
+
+#Stripping white_space
+print(middle.rstrip())
+print(middle.lstrip())
+print(middle.rstrip())
+
+print(middle.split())
